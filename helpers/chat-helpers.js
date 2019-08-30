36587fbaf5aaf252f103f5fed397d98d17488924
josh096 <joshw096@gmail.com>
2019-08-30 15:15:45 +0100

    Working weather bot

---
+++ helpers/chat-helpers.js
@@ -0,0 +1,13 @@
+const formatChat = week => {
+    return week.map(d => {
+        const date = new Date(d.day);
+        const day = date.toString().substring(0, 4);
+        const temp = d.temp;
+        const desc = d.desc;
+        return `${day} - ${desc} - ${temp}°`;
+    });
+};
+
+module.exports = {
+    formatChat
+};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const axios = require('axios');
 const config = require('./config.json');
+const _ = require('lodash');
+const {formatChat} = require('./helpers/chat-helpers');
 
 const bot = new Discord.Client();
 
@@ -20,18 +22,31 @@ bot.on('message', message => {
                 message.channel.send('Pong!');
                 break;
             case 'weather':
-                console.log(args);
                 const city = args[0];
                 const country = args[1];
-                axios
-                    .get(
-                        `http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&APPID=${config.weatherkey}`
-                    )
-                    .then(res => {
-                        console.log(res);
-                        message.channel.send(JSON.stringify(res));
-                    });
-                break;
+                try {
+                    axios
+                        .get(
+                            `http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&units=metric&APPID=${config.weatherkey}`
+                        )
+                        .then(res => {
+                            const list = res.data.list;
+                            const days = [];
+                            list.map(d => {
+                                let day = d.dt_txt.substr(0, 10);
+                                days.push({
+                                    day: day,
+                                    temp: d.main.temp,
+                                    desc: d.weather[0].main
+                                });
+                            });
+                            const week = _.uniqBy(days, 'day');
+                            message.channel.send(formatChat(week));
+                        });
+                    break;
+                } catch (err) {
+                    console.error(err);
+                }
         }
     }
 });
